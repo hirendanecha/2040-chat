35401ab3fc7d0dc134f02903f08f16eeccfc2f93
vatsal <vso.opash@gmail.com>
2024-09-25 11:20:51 +0530

    improve notification

--- src/app/layouts/main-layout/pages/notifications/notification.component.ts
+++ src/app/layouts/main-layout/pages/notifications/notification.component.ts
@@ -69,16 +69,16 @@ export class NotificationsComponent {
         this.toastService.success(
           res.message || 'Notification delete successfully'
         );
-        this.getNotificationList();
+        this.notificationList = this.notificationList.filter(notification => notification.id !== id);
       },
     });
   }
 
-  readUnreadNotification(id, isRead): void {
-    this.customerService.readUnreadNotification(id, isRead).subscribe({
+  readUnreadNotification(notification, isRead): void {
+    this.customerService.readUnreadNotification(notification.id, isRead).subscribe({
       next: (res) => {
         this.toastService.success(res.message);
-        this.getNotificationList();
+        notification.isRead = isRead;
       },
     });
   }
